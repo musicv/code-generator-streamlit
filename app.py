28ce100c5d23ee12988ad1c4c67982cfa415cb91
Vivijana Music <127100112+musicv@users.noreply.github.com>
2024-12-04 10:47:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,6 +48,15 @@ st.write("Enter a description of the Python application or code you need. CodeLl
 # Input box for the user to enter a description
 description = st.text_area("Application or Code Description", placeholder="Describe the application or code you want")
 
+# Insert containers separated into tabs:
+>>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+>>> tab1.write("this is tab 1")
+>>> tab2.write("this is tab 2")
+
+# You can also use "with" notation:
+>>> with tab1:
+>>>   st.radio('Select one:', [1, 2])
+
 # Button to trigger code generation
 if st.button("Generate Code"):
     if description.strip():
